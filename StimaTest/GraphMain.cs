a4abdc37cd86eb7cad8ba44d4ef5ec536dc1fd61
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2021-03-18 01:30:40 +0700

    added graph class

---
+++ StimaTest/GraphMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace GraphMain
+{
+    public class Graph
+    {
+        //Fields
+        private Node first;
+
+        //Constructors
+        public Graph(string X)
+        {
+            Node P = new Node(X);
+            this.first = P;
+        }
+
+        //Getter and Setter
+        public Node First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+
+
+        //Methods
+        public Node SearchNode(string X)
+        {
+            Node P;
+            P = first;
+            while (P != null && P.Id != X)
+            {
+                P = P.Next;
+            }
+            return P;
+        }
+
+        public SuccNode SearchEdge(string prec, string succ)
+        {
+            Node P;
+            SuccNode T;
+
+            P = SearchNode(prec);
+
+            if (P == null) { return null; }
+            else
+            {
+                T = P.Trail;
+                if (T == null) { return null; }
+                else
+                {
+                    while ((T.Succ).Id != succ && (T.NextT != null))
+                    {
+                        T = T.NextT;
+                    }
+                    if (T.Succ.Id != succ) { return null; }
+                    else { return T; }
+                }
+            }
+        }
+
+        public void InsertNode(string X)
+        {
+            Node LastG;
+
+            LastG = first;
+            Node Pn = new Node(X);
+            if (Pn != null)
+            {
+                while (LastG.Next != null)
+                {
+                    LastG = LastG.Next;
+                }
+                LastG.Next = Pn;
+            }
+
+        }
+
+        //Can only be used for nodes with NPred = 0 for now
+        public void DeleteNode(string X)
+        {
+            Node P;
+            Node Prev;
+
+            P = SearchNode(X);
+            Prev = first;
+
+            //removing from main list
+            if (P != null)
+            {
+                if (P == first)
+                {
+                    first = P.Next;
+                } else
+                {
+                    while (Prev.Next != P)
+                    {
+                        Prev = Prev.Next;
+                    }
+                    Prev.Next = P.Next;
+                }
+
+                //Removing all edges that P is connected to
+                DeleteAllEdges(P);
+            }
+        }
+
+        public void InsertEdge(string prec, string succ)
+        {
+            Node Pprec = SearchNode(prec);
+            Node Psucc = SearchNode(succ);
+            SuccNode T;
+
+            if (SearchEdge(prec, succ) == null)
+            {
+                T = Pprec.Trail;
+                if (T == null)
+                {
+                    SuccNode temp = new SuccNode(Psucc);
+                    Pprec.Trail = temp;
+                } else
+                {
+                    while (T.NextT != null)
+                    {
+                        T = T.NextT;
+                    }
+                    SuccNode temp = new SuccNode(Psucc);
+                    T.NextT = temp;
+                }
+                Psucc.NPred += 1;
+            }
+
+        }
+
+        public void DeleteAllEdges(Node Pn)
+        {
+            SuccNode Pt;
+
+            Pt = Pn.Trail;
+            while (Pt != null)
+            {
+                (Pt.Succ).NPred -= 1;
+                Pt = Pt.NextT;
+            }
+        }
+
+    }
+
+    public class Node
+    {
+        //Fields
+        private int nPred;
+        private string id;
+        private Node next;
+        private SuccNode trail;
+
+        //Constructors
+        public Node(string _Id)
+        {
+            this.id = _Id;
+            this.nPred = 0;
+            this.next = null;
+            this.trail = null;
+        }
+
+        //Getter and Setter
+        public int NPred
+        {
+            get { return nPred; }
+            set { nPred = value;  }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value;  }
+        }
+
+        public Node Next
+        {
+            get { return next; }
+            set { next = value;  }
+        }
+
+        public SuccNode Trail
+        {
+            get { return trail; }
+            set { trail = value; }
+        }
+
+
+        //Methods
+    }
+
+    public class SuccNode
+    {
+        //Fields
+        private Node succ;
+        private SuccNode nextT;
+
+        //Constructors
+        public SuccNode(Node Pn)
+        {
+            this.succ = Pn;
+        }
+
+        //Getter and Setter
+        public Node Succ
+        {
+            get { return succ; }
+            set { succ = value; }
+        }
+
+        public SuccNode NextT
+        {
+            get { return nextT; }
+            set { nextT = value; }
+        }
+
+    }
+}
---
+++ StimaTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using GraphMain;
+
+namespace Main
+{
+    class DriverGraph
+    {
+        static void Main(string[] args)
+        {
+
+            string[] ids = new string[5] { "C1", "C2", "C3", "C4", "C5" };
+
+            Graph g = new Graph("C1");
+            for (int i = 1; i < 5; i++)
+            {
+                g.InsertNode(ids[i]);
+            }
+
+            g.InsertEdge("C1", "C2"); //Artinya: menambahkan busur berarah dari C1 KE C2
+            g.InsertEdge("C1", "C5");
+            g.InsertEdge("C2", "C5");
+            g.InsertEdge("C2", "C3");
+            g.InsertEdge("C2", "C4");
+            g.InsertEdge("C3", "C2");
+            g.InsertEdge("C3", "C4");
+            g.InsertEdge("C4", "C2");
+            g.InsertEdge("C4", "C5");
+            g.InsertEdge("C4", "C3");
+            g.InsertEdge("C5", "C4");
+            g.InsertEdge("C5", "C2");
+            g.DeleteNode("C1");
+
+            Node cP = g.First;
+            while (cP != null)
+            {
+                Console.WriteLine("Terdapat {0} busur terhubung ke node dengan Id = {1}", cP.NPred, cP.Id);
+                cP = cP.Next;
+            }
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
